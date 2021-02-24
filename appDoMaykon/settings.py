9b03dbc30e01767ef18d1074117545528063cf0c
marcosdcl <marcosdouglasclima@gmail.com>
2021-02-24 16:26:41 -0300

    Add database settings

--- appDoMaykon/settings.py
+++ appDoMaykon/settings.py
@@ -74,15 +74,16 @@ WSGI_APPLICATION = 'appDoMaykon.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': '5433',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
